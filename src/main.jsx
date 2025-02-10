0d4d02643a4f8b166c815561982ed2be96367023
rajeenafazil <rajeena@ipixsolutions.com>
2025-02-10 15:07:36 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom"; // ✅ Router should be here
+import { BrowserRouter as Router } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import App from "./App";
 import "./index.css";
